c014f47dc8c033f24e81acef4c2db80d5dc2b4f3
Marcs-paulo <mp.dantasjoaquim0102@gmail.com>
2025-09-09 21:58:00 -0300

    Atualiza models e configurações do admin

--- pagina/models.py
+++ pagina/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 # só para teste
-class TestModel(models.Model):
-    name = models.CharField(max_length=100)
+class SiteConfiguration(models.Model):
+    endereco = models.CharField(max_length=255)
+    telefone = models.CharField(max_length=20)
+    email = models.EmailField()
+    instagram = models.URLField(null=True, blank=True)
+    facebook = models.URLField(null=True, blank=True)
+    youtube = models.URLField(null=True, blank=True)
 
+    def __str__(self):
+        return "Configuração do Site"
+    
